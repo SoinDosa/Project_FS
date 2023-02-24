924b0ebf489f82e1d6e8e1bdbc4a9f1bc8358219
SoinDosa <rlatjsgnl519@gmail.com>
2023-02-24 16:43:27 +0900

    [feat] 게임 종료 기능

---
+++ Assets/Scripts/UI/Popup/QuitPopup.cs
@@ -0,0 +1,29 @@
+using PFS.UI.Common.popupBase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PFS.UI.Popup.quitPopup
+{
+    public class QuitPopup : PopupBase
+    {
+        public override void OnOpenPopup()
+        {
+            base.OnOpenPopup();
+        }
+
+        public override void OnClosePopup()
+        {
+            base.OnClosePopup();
+        }
+
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
