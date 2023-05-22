bb0328b3b7961749be44f886c7a727ef5c0bf56f
SoinDosa <rlatjsgnl519@gmail.com>
2023-05-22 19:23:52 +0900

    [feat] GameScene UI 점수판 생성

--- Assets/Scripts/UI/GameScene/GameScene.cs
+++ Assets/Scripts/UI/GameScene/GameScene.cs
@@ -1,4 +1,7 @@
+using PFS.GamePlay.Rule.gameManager;
 using PFS.UI.Common.popupBase;
+using System;
+using TMPro;
 using UnityEngine;
 
 namespace PFS.UI.GameScene.gameScene
@@ -7,6 +10,7 @@ namespace PFS.UI.GameScene.gameScene
     {
         [SerializeField] private PopupBase _pausePopup;
         [SerializeField] private PopupBase _gameOverPopup;
+        [SerializeField] private TextMeshProUGUI _scoreText;
 
         void Update()
         {
@@ -14,11 +18,23 @@ namespace PFS.UI.GameScene.gameScene
             {
                 _pausePopup.OnOpenPopup();
             }
+
+            CheckScore();
         }
 
         public void OpenGameOverPopup(int msg)
         {
             _gameOverPopup.OnOpenPopup(msg);
+        }
+
+        private void CheckScore()
+        {
+            if (Convert.ToUInt64(_scoreText.text) == GameManager.totalScore)
+            {
+                return;
+            }
+
+            _scoreText.text = GameManager.totalScore.ToString();
         }
     }
 
